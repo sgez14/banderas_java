d25af58a3fe3b859203f612a54dcbe9fcaf70b7c
sgez14 <samir.colpas@hotmail.com>
2020-10-21 12:22:29 -0500

    se arreglo el bucle y la funcion para colorear

--- Juego.java
+++ Juego.java
@@ -22,7 +22,27 @@ public class Juego{
 	 		if(fila[i].equals("4"))
 	 		{
 	 			System.out.print(ConsoleColors.YELLOW_BACKGROUND+"  ");
-	 		}		 		
+	 		}
+	 		if(fila[i].equals("5"))
+	 		{
+	 			System.out.print(ConsoleColors.GREEN_BACKGROUND+"  ");
+	 		}
+	 		if(fila[i].equals("6"))
+	 		{
+	 			System.out.print(ConsoleColors.PURPLE_BACKGROUND+"  ");
+	 		}
+	 		if(fila[i].equals("7"))
+	 		{
+	 			System.out.print(ConsoleColors.CYAN_BACKGROUND+"  ");
+	 		}
+	 		if(fila[i].equals("8"))
+	 		{
+	 			System.out.print(ConsoleColors.BLACK_BACKGROUND+"  ");
+	 		}
+	 		if(fila[i].equals("9"))
+	 		{
+	 			System.out.print(ConsoleColors.PURPLE_BACKGROUND+"  ");
+	 		}	 		
 		}
 		System.out.println(ConsoleColors.RESET);
 	}
@@ -47,11 +67,30 @@ public class Juego{
 				{
 					System.out.println(ConsoleColors.RED + "Mal");
 					System.out.println("La respuesta es: " + fila[0].toLowerCase());
-					System.out.println(ConsoleColors.RESET)
+					System.out.println(ConsoleColors.RESET);
 				}	
 			}
 		}
 		return prueba;				
+	}
+	public static void menuSaludos(int puntaje_juego)
+	{
+		if(puntaje_juego==1)
+		{
+			System.out.println(ConsoleColors.CYAN + "Primer punto");
+		}
+		else if(puntaje_juego==2)
+		{
+			System.out.println(ConsoleColors.CYAN + "Un punto mas!");
+		}
+		else if(puntaje_juego==3)
+		{
+			System.out.println(ConsoleColors.GREEN + "Eso es todo");
+		}
+
+		System.out.println(ConsoleColors.RESET);
+			
+
 	}
 
 	public static void imprimirGraficoBandera(String[] banderas, int indice)
@@ -70,6 +109,29 @@ public class Juego{
 			contador+=20;		
 		}
 		return indices;
+	}
+	public static void creditos()
+	{
+		System.out.println();
+		System.out.println("  /$$$$$$                          /$$           ");
+       	System.out.println(" /$$__  $$                        |__/           ");
+       	System.out.println("| $$  \\__/  /$$$$$$  /$$$$$$/$$$$  /$$  /$$$$$$  ");
+       	System.out.println("|  $$$$$$  |____  $$| $$_  $$_  $$| $$ /$$__  $$ ");
+       	System.out.println(" \\____  $$  /$$$$$$$| $$ \\ $$ \\ $$| $$| $$  \\__/ ");
+       	System.out.println(" /$$  \\ $$ /$$__  $$| $$ | $$ | $$| $$| $$       ");
+       	System.out.println("|  $$$$$$/|  $$$$$$$| $$ | $$ | $$| $$| $$       ");
+       	System.out.println(" \\______/  \\_______/|__/ |__/ |__/|__/|__/       ");
+       	System.out.println("                                                 ");
+	
+	
+       	System.out.println(" $$\\   $$\\                         $$\\                            ");
+       	System.out.println(" $$ |  $$ |                        $$ |                           ");
+       	System.out.println(" $$ |  $$ |$$\\   $$\\ $$$$$$\\$$$$\\  $$$$$$$\\   $$$$$$\\   $$$$$$\\   ");
+       	System.out.println(" $$$$$$$$ |$$ |  $$ |$$  _$$  _$$\\ $$  __$$\\ $$  __$$\\ $$  __$$\\  ");
+       	System.out.println(" $$  __$$ |$$ |  $$ |$$ / $$ / $$ |$$ |  $$ |$$$$$$$$ |$$ |  \\__| ");
+       	System.out.println(" $$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |$$   ____|$$ |       ");
+       	System.out.println(" $$ |  $$ |\\$$$$$$  |$$ | $$ | $$ |$$$$$$$  |\\$$$$$$$\\ $$ |       ");
+       	System.out.println(" \\__|  \\__| \\______/ \\__| \\__| \\__|\\_______/  \\_______|\\__|       ");
 	}
 	public static void juego()
 	{
@@ -93,7 +155,7 @@ public class Juego{
 			switch(centinela)
 			{
 				case 1: System.out.println();
-						while(puntaje_juego<3)
+						while(puntaje_juego!=3)
 						{
 							indice_bandera = indiceAleatorio.nextInt(indices.length);
 							opcion_bandera = indice_bandera;
@@ -112,14 +174,19 @@ public class Juego{
 									{
 										System.out.println("Adivina 3 banderas para ganar");
 									}
-									if(puntaje_juego==2)
-									{
-										System.out.println(ConsoleColors.CYAN + "Ya casi ganas!");
-									}
+									menuSaludos(puntaje_juego);
 								}							
 							}
-						}		  																										
-			}		
+						}
+						puntaje_juego=0;
+						break;
+						
+				case 2: creditos();
+       					break;
+       			case 3: System.out.println("Adios :)");
+       					break;
+       			default: System.out.println("Opcion invalida");	  																										
+			}			
 
 		}while(centinela!=3);
 
